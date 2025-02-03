44ddf7fd8fbc1906895d04a4054defc06fc2e81a
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2025-02-03 18:52:34 +0200

    Update file

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def root():
     return {"message": "Welcome to FastAPI"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, workers=4)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=4)
 
 
 @app.exception_handler(RateLimitExceeded)
